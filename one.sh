6b4a0aebd71e5bf53c0dee33ec1909c4869fb334
YaseminYakar <yasemin.yakar@outlook.com>
2020-12-29 16:08:07 -0500

    Update one.sh

--- one.sh
+++ one.sh
@@ -23,3 +23,39 @@ My expectations are high. I expect a great project from you guys."
 
 Code Submission (Code Freeze): January 28th, 2021 by 11:59 PM
 Project Presentation: January 29th, 2021 @9:00 AM (edited)
+
+
+
+PIZZA SIZE
+
+#!/bin/bash
+
+counter=1
+pizza_size=(small medium large xlarge)
+read -p "Please enter your name for your order >>" name
+echo "Hello $name, welcome to Great Pizza Place! Please choose a size. "
+echo "--------------------------------"
+for t in ${pizza_size[@]}
+do
+echo "$counter. $t"
+((counter ++))
+done
+echo "--------------------------------"
+echo ""
+
+while read -p "Choose one [1-4] >>" selection
+
+do case $selection in
+
+1) echo "You have choosen ${pizza_size[0]}."
+break;;
+2) echo "You have choosen ${pizza_size[1]}."
+break;;
+3) echo "You have choosen ${pizza_size[2]}."
+break;;
+4) echo "You have choosen ${pizza_size[3]}."
+break;;
+*) echo "You didn't choose a correct size, please try again.";;
+
+esac
+done
